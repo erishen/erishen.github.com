db99633d1cd604647ce0eb9136ab8554c4c2fc87
Erishen <lei_sun@trip.com>
2023-07-31 22:06:01 +0800

    add next13-skeleton

---
+++ public/next13-skeleton/_next/static/chunks/app/menu/[id]/page-4ee1dd03bb168088.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[959],{3490:function(e,n,t){Promise.resolve().then(t.t.bind(t,3222,23)),Promise.resolve().then(t.t.bind(t,6685,23)),Promise.resolve().then(t.t.bind(t,9193,23))},9193:function(){}},function(e){e.O(0,[685,222,971,596,744],function(){return e(e.s=3490)}),_N_E=e.O()}]);
