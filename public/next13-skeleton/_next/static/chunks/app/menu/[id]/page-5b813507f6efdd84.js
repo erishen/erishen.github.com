e98f81ff1cecd142244b01cffe51e1959a14e752
Erishen <lei_sun@trip.com>
2023-08-01 10:47:06 +0800

    update

--- public/next13-skeleton/_next/static/chunks/app/menu/[id]/page-5b813507f6efdd84.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[959],{2555:function(e,n,t){Promise.resolve().then(t.t.bind(t,6685,23)),Promise.resolve().then(t.t.bind(t,3222,23)),Promise.resolve().then(t.t.bind(t,9193,23))},9193:function(){}},function(e){e.O(0,[685,222,971,596,744],function(){return e(e.s=2555)}),_N_E=e.O()}]);
